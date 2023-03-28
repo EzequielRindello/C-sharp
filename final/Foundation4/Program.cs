fa1957140b61aa00afdcbf3b6706c5dfb0bea218
EzequielRindello <rindelloezequiel@gmail.com>
2023-03-28 14:12:12 -0300

    .

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -2,14 +2,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Clear the console and create a list of activities
+        // Clear the console, create a list of activities, and initialize a variable i to keep track of the count of activities.
         int i = 0;
         Console.Clear();
         List<Activity> activities = new List<Activity>();
         Console.WriteLine("Program 4: Polymorphism with Exercise Tracking.");
         Console.WriteLine();
 
-        // Create some activities and add to the list of activities
+        // Create some activities and add to the list of activities.
         Running running = new Running("27/3/2023", 30, 3, "Running");
         activities.Add(running);
 
@@ -19,7 +19,7 @@ class Program
         Swimming swimming = new Swimming("29/3/2023", 60, 25, "Swimming");
         activities.Add(swimming);
 
-        // Iterate through the list of activities
+        // Iterate through the list of activities.
         foreach (Activity activity in activities)
         {
             i++;
